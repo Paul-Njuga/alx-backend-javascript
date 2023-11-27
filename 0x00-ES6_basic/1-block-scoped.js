dca4aa34c71d3b31b42b15f239e909cccb283c33
Paul Njuga <paulnjuga@gmail.com>
2023-11-28 00:57:14 +0300

    Fix: disable eslint for unused vars

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,13 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /*
+    Lets disable eslint for the next two lines,
+    so it doesn't cry about unused consts
+    */
+    // eslint-disable-next-line
     const task = true;
+    // eslint-disable-next-line
     const task2 = false;
   }
   return [task, task2];
