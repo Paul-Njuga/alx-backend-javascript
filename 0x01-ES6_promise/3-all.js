2bd5d945eb32eb45be71e38483581893c4ba426b
Paul Njuga <paulnjuga@gmail.com>
2023-11-29 20:33:11 +0300

    Add all promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,27 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  /* Array of promises */
+  const promises = [uploadPhoto(), createUser()];
+
+  /* Promise.all() collectively resolves all promises */
+  Promise.all(promises)
+    .then((data) => {
+      /*
+       * `data` fmt:
+       * [
+       *   { status: 200, body: 'photo-profile-1' },
+       *   { firstName: 'Guillaume', lastName: 'Salva' }
+       * ]
+       */
+
+      /* Deconstruct data into photoData & userData */
+      const [photoData, userData] = data;
+
+      /* Extract the relevant info */
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
