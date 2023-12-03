fb6f6b3954bbd9942ffedc4a40fd0222b16761f7
Paul Njuga <paulnjuga@gmail.com>
2023-12-03 22:45:15 +0300

    Add jsdoc for better commenting

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,14 @@
+/**
+ * Represents a classroom.
+ * @class
+ */
 export default class ClassRoom {
+  /**
+   * Create a classroom.
+   * @constructor
+   * @param {number} maxStudentSize,
+   * - The max number of students per classroom.
+   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
