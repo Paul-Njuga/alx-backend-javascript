f380879f608ed6f42bc0424428d2eefffcf3827e
Paul Njuga <paulnjuga@gmail.com>
2023-12-05 01:54:37 +0300

    Add override super class methods

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,38 @@
+/**
+ * Represents a building
+ * @class
+ */
+
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  /**
+   * Initializes Building
+   * @constructor
+   * @param {number} sqft
+   */
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new TypeError('Square feet must be a number');
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  /**
+   * Getter method for sqft.
+   * @returns {number} - sqft
+   */
+  get sqft() {
+    return this._sqft;
+  }
+
+  /**
+   * Abstract method that must be implemented by subclasses.
+   * Throws an error if not overridden.
+   * @abstract
+   * @throws {Error} - If not overridden by subclass.
+   */
+
+  /* eslint-disable-next-line class-methods-use-this */
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
