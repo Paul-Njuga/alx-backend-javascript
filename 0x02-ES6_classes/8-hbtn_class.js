4695bcf20b49d19f7ccbcdd61a64bfe74626ac16
Paul Njuga <paulnjuga@gmail.com>
2023-12-05 02:56:22 +0300

    Override toSting() & toValue()

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,37 @@
+/**
+ * Represents HolbertonClass.
+ * @class
+ */
+
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonClass {
+  /**
+   * Constructor for the HolbertonClass.
+   * @param {Number} size
+   * @param {String} location
+   */
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  /**
+   * Conversion to Number.
+   * @description The valueOf method is overridden to provide,
+   * the behavior when the class is cast into a Number.
+   * @returns {Number} size
+   */
+  valueOf() {
+    return this._size;
+  }
+
+  /**
+   * Conversion to String.
+   * @description The toString method is overridden to provide,
+   * the behavior when the class is cast into a String.
+   * @returns {String} location
+   */
+  toString() {
+    return this._location;
+  }
+}
