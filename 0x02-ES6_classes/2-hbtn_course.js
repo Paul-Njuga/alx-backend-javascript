0365fdaed0089d21741504db459b793120a2ea18
Paul Njuga <paulnjuga@gmail.com>
2023-12-04 12:24:14 +0300

    Disable eslint warning
    Disable eslint no-underscore-dangle. Rearrange getter & setter methods for better readability.

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,6 +2,8 @@
  * Represents an alx course.
  * @class
  */
+
+/* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   /**
    * Create a course.
@@ -21,33 +23,30 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
-  /* Getter methods for each attribute */
-  get name() {
-    return this._name;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
-  }
-
   /**
+   * Getter methods for each attribute.
    * Setter methods for each attribute with type validation.
    * @param {string|number|Array} value - The value to set.
    */
+  get name() {
+    return this._name;
+  }
   set name(value) {
     if (typeof value === 'string') this._name = value;
     else throw new TypeError('Name must be a string');
   }
 
+  get length() {
+    return this._length;
+  }
   set length(value) {
     if (typeof value === 'number') this._length = value;
     else throw new TypeError('Length must be a number');
   }
 
+  get students() {
+    return this._students;
+  }
   set students(value) {
     if (Array.isArray(value)) this._length = value;
     else throw new TypeError('Students must be an array');
