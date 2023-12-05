09197c878ca84e6f28f028c46c03e7dd553e4b27
Paul Njuga <paulnjuga@gmail.com>
2023-12-05 11:26:43 +0300

    Add array mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+/**
+ * Gets a list of student's IDs.
+ * @param {Array<object>}
+ * returns {Array<string>}
+ */
+export default function getListStudentIds(array) {
+  if (!Array.isArray(array)) return [];
+  return array.map(student => student.id);
+}
