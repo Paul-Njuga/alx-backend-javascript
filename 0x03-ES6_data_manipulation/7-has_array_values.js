ed83eb7378b86b6d979535afe82dcd082ecbfe5f
Paul Njuga <paulnjuga@gmail.com>
2023-12-05 22:06:00 +0300

    Add set has array

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,9 @@
+/**
+ * Checks for an array within a set.
+ * @param {Set} set
+ * @param {Array} array
+ * returns {boolean}
+ */
+export default function hasValuesFromArray(set, array) {
+  return array.every(elem => set.has(elem));
+}
