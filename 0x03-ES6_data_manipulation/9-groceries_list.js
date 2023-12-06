6a93a190820cd70fc9c43e1807d2826ff03b896e
Paul Njuga <paulnjuga@gmail.com>
2023-12-06 03:18:52 +0300

    Add mapped array

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+ * Create a map for groceries.
+ * @returns {Map} - groceries
+ */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasts', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
