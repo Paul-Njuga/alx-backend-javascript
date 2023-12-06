cc96bf831fa68dc392eb171449e44cf25b63ce39
Paul Njuga <paulnjuga@gmail.com>
2023-12-06 03:33:00 +0300

    Fix: add return value

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,11 @@
 /**
  * Updates map values.
  * @param {Map} - map
+ * @returns {Map} - updated map
  */
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) throw new Error('Cannot process');
-  map.forEach((value, key) => {
+  return map.forEach((value, key) => {
     if (value === 1) map.set(key, 100);
   });
 }
