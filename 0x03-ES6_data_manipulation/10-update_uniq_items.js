4989369b0f75d58550f3de96600d25c00dee7a8c
Paul Njuga <paulnjuga@gmail.com>
2023-12-06 03:35:43 +0300

    Fix: rmv return value

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,10 @@
 /**
  * Updates map values.
  * @param {Map} - map
- * @returns {Map} - updated map
  */
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) throw new Error('Cannot process');
-  return map.forEach((value, key) => {
+  map.forEach((value, key) => {
     if (value === 1) map.set(key, 100);
   });
 }
