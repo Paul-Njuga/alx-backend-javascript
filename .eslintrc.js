bebd6a744894bdffa91ff580e5f7f8190bfa67fe
Paul Njuga <paulnjuga@gmail.com>
2023-12-07 03:50:58 +0300

    Rmv & separate ts dependencies from js dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,29 @@
 module.exports = {
- env: {
-   browser: false,
-   es6: true,
-   jest: true,
-   node: true,
- },
- parser:  '@typescript-eslint/parser',
- extends: [
-   'airbnb-base',
-   'plugin:jest/all',
-   'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
- ],
- globals: {
-   Atomics: 'readonly',
-   SharedArrayBuffer: 'readonly',
- },
- parserOptions: {
-   ecmaVersion: 2018,
-   sourceType: 'module',
- },
- plugins: ['jest'],
- rules: {
-   'no-console': 'off',
-   'no-shadow': 'off',
-   'no-restricted-syntax': [
-     'error',
-     'LabeledStatement',
-     'WithStatement',
-   ],
- },
- overrides:[
-   {
-     files: ['*.js'],
-     excludedFiles: 'babel.config.js',
-   }
- ]
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['airbnb-base', 'plugin:jest/all'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    },
+  ],
 };
